ddad853b495d38e80bc58884ea2f4031ff991180
UZBLEADERRR <sarvarbeksanjarivich@example.com>
2022-04-26 11:34:27 +0500

    Buni bemalol sinab ko'rishingiz mumkin!

---
+++ app.js
@@ -0,0 +1,59 @@
+
+// oq_qatlam.addEventListener("click", () => {
+//     night.style.display = "block"
+// })
+
+
+night_club.addEventListener("click", () =>{
+    night_club.style.display = "none"
+    night.style.display = "none"
+    day_club.style.display = "block"
+    day.style.display = "block"
+    body.style.backgroundColor = "white"
+    day.style.backgroundColor = "rgb(37, 35, 35)"
+})
+
+day_club.addEventListener("click", () =>{
+    night_club.style.display = "block"
+    night.style.display = "block"
+    day.style.display = "none"
+    day_club.style.display = "none"
+    body.style.backgroundColor = "rgb(37, 35, 35)"
+    day.style.backgroundColor = "white"
+})
+
+// oq_qatlam.addEventListener("click", () =>{
+//     night.style.display = "none"
+//     day.style.display = "block"
+//     body.style.backgroundColor = "white"
+//     day.style.backgroundColor = "rgb(37, 35, 35)"
+//     ustki_qatlam.idName = "oq_qatlam"
+//     // oq_qatlam.idName = "bigelement"
+// })
+
+// oq_qatlam.className = "bigelement"
+
+// let bigelement1 = document.querySelector(".bigelement");
+
+// bigelement1.addEventListener("click", () =>{
+//     night.style.display = "block"
+//     day.style.display = "none"
+//     body.style.backgroundColor = "rgb(37, 35, 35)"
+//     day.style.backgroundColor = "white"
+// })
+
+// bigelement1.addEventListener("click", () =>{
+//     night.style.display = "none"
+//     day.style.display = "block"
+//     body.style.backgroundColor = "white"
+//     day.style.backgroundColor = "rgb(37, 35, 35)"
+//     // oq_qatlam.idName = "bigelement"
+// })
+
+// ustki_qatlam.addEventListener("click", () =>{
+//     night.style.display = "block"
+//     day.style.display = "none"
+//     body.style.backgroundColor = "rgb(37, 35, 35)"
+//     day.style.backgroundColor = "white"
+// })
+
